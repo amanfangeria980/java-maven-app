4edb340f4da37557a0080a7cc8a41f6d3990c067
amanfangeria980 <amanfangeria980@gmail.com>
2023-03-25 19:09:55 +0530

    changed credentials

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image and pushing it to docker hub..."
     withCredentials([usernamePassword(credentialsId: 'Docker-Hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    sh 'docker build -t amanfangeria/java-maven-app:2.0.0 .'
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'docker push amanfangeria/java-maven-app:2.0.0'
     }
 } 
 
