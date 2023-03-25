544f7989105db57a79a28fb72d394425aad3fe10
amanfangeria980 <amanfangeria980@gmail.com>
2023-03-25 19:13:04 +0530

    corrected variable names

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image and pushing it to docker hub..."
     withCredentials([usernamePassword(credentialsId: 'Docker-Hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     sh 'docker build -t amanfangeria/java-maven-app:2.0.0 .'
-    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
     sh 'docker push amanfangeria/java-maven-app:2.0.0'
     }
 } 
